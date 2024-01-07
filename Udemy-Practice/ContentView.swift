e404456a3183ba5eac0d5579eb44536cb101b84e
Aellianna <155892543+Aellianna@users.noreply.github.com>
2024-01-07 17:14:07 +0300

    Initial Commit

---
+++ Udemy-Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Udemy-Practice
+//
+//  Created by Aellianna L. Leone on 07/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udemy-Practice/Udemy_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udemy_PracticeApp.swift
+//  Udemy-Practice
+//
+//  Created by Aellianna L. Leone on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Udemy_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
